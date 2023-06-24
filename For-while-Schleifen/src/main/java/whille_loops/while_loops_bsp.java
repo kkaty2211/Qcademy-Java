e9fe26bee10c9534761f5d8208b1b8356e6ef819
Katharina K <k.kerschbaumer@hotmail.com>
2023-06-24 13:07:42 +0200

    Version 2: Test

--- For-while-Schleifen/src/main/java/whille_loops/while_loops_bsp.java
+++ For-while-Schleifen/src/main/java/whille_loops/while_loops_bsp.java
@@ -1,7 +1,11 @@
 package whille_loops;
 
+import java.util.Arrays;
+
 public class while_loops_bsp {
 
+
+    //gibt die Fakultät einer Zahl wieder
     public static long fakultaetWhile(int n) { // long da mit Fakultaet das Ergebnis sehr schnell sehr groß werden kann
         long result = 1;
         int a = 1;
@@ -19,10 +23,55 @@ public class while_loops_bsp {
         }
         return result;
     }
+
+
+    // gibt die Summer eines Arrays wieder
+    public static double array_summe(double[] arr) {
+        double result = 0;
+        int i = 0;
+        while (i < arr.length) {
+            result += arr[i];
+            i ++;
+        }
+        return result;
+    }
+
+
+
+    //gibt die Primzahlen bis n wieder
+    public static void primeNumbers(int n) {;
+        int i = 2;
+        while (i<= n) {
+            if (isPrime(i)) {
+                System.out.println(i);;
+            }
+            i++;
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        int i = 2;
+        while (i <= Math.sqrt(n)) {
+            if (n % i == 0) {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+
+
+
+
     public static void main(String[] args) {
         int n = 9;
-        System.out.println(n + "! (While Schleifr) = " + fakultaetWhile(n));
+        System.out.println(n + "! (While Schleife) = " + fakultaetWhile(n));
         System.out.println(n + "! (For Schleife) = " + fakultaetFor(n));
+        double[] arr = {9,2,5};
+        System.out.println( "Die Summe des Arryas " + Arrays.toString(arr) + " lautet: "+ array_summe(arr));
+        int b = 47;
+        System.out.println("Die Primzahlen bis " + b + " sind folgende: ");
+        primeNumbers(b);
 
     }
 }
